f5a3721ff5232b741f288d44781b65218c3de7de
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-10-24 01:07:20 -0300

    fix(Address): adicionar construtor

--- src/br/edu/ifpi/poo/entities/Address.java
+++ src/br/edu/ifpi/poo/entities/Address.java
@@ -6,4 +6,12 @@ public class Address {
     private String neighborhood;
     private String city;
     private String state;
+
+    public Address(String street, String number, String neighborhood, String city, String state) {
+        this.street = street;
+        this.number = number;
+        this.neighborhood = neighborhood;
+        this.city = city;
+        this.state = state;
+    }
 }
