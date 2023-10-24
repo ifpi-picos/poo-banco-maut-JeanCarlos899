b36b5a426af7e95616bfc84a368c5940f4a646b1
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-10-24 00:59:54 -0300

    fix(SavingsAccount): corrigindo

--- src/br/edu/ifpi/poo/entities/SavingsAccount.java
+++ src/br/edu/ifpi/poo/entities/SavingsAccount.java
@@ -7,23 +7,29 @@ public class SavingsAccount extends Account {
 
     @Override
     public void deposit(double value) {
-        // rendimento de 10% por depósito
-        double depositValue = value * 1.1;
-        super.balance += depositValue;
+        if (value > 0) {
+            // rendimento de 10% por depósito
+            double depositValue = value * 1.1;
+            super.balance += depositValue;
+        }
     }
 
     @Override
     public void withdraw(double value) {
-        // taxa de 5% por saque
-        double withdrawValue = value * 1.05;
-        super.balance -= withdrawValue;
+        if (value > 0 && value <= super.balance) {
+            // taxa de 5% por saque
+            double withdrawValue = value * 1.05;
+            super.balance -= withdrawValue;
+        }
     }
 
     @Override
     public void transfer(Account destinationAccount, double value) {
-        // taxa de 10% por transferência
-        double transferValue = value * 1.1;
-        withdraw(transferValue);
-        destinationAccount.deposit(value);
+        if (value > 0 && value <= super.balance) {
+            // taxa de 10% por transferência
+            double transferValue = value * 1.1;
+            withdraw(transferValue);
+            destinationAccount.deposit(value);
+        }
     }
 }
