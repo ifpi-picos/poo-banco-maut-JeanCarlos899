760a9a9d3f5d2185aba35826c1eeb44adf5b6c56
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-10-25 00:12:49 -0300

    refactory(NotificationsSms): deixar em inglês

--- src/br/edu/ifpi/poo/notifications/NotificationsSms.java
+++ src/br/edu/ifpi/poo/notifications/NotificationsSms.java
@@ -2,9 +2,9 @@ package br.edu.ifpi.poo.notifications;
 
 public class NotificationsSms implements Notifications {
     @Override
-    public void enviaNotificacao(String tipo, double valor) {
+    public void sendNotification(String type, double value) {
         System.out.println("Enviando notificação por SMS");
-        System.out.println("Tipo: " + tipo);
-        System.out.println("Valor: " + valor);
+        System.out.println("Tipo: " + type);
+        System.out.println("Valor: " + value);
     }
 }
