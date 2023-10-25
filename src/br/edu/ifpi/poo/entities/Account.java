351820f1a183454e93ed6079a39c90c945b90671
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-10-25 00:09:00 -0300

    style(Account): organizando código

--- src/br/edu/ifpi/poo/entities/Account.java
+++ src/br/edu/ifpi/poo/entities/Account.java
@@ -1,23 +1,27 @@
 package br.edu.ifpi.poo.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifpi.poo.notifications.Notifications;
+
 public abstract class Account {
+    protected List<Transaction> transactions = new ArrayList<>();
+
     protected final String agency;
     protected final String accountNumber;
+    protected Notifications notifications;
     protected double balance = 0;
     private Client client;
 
+    // variável estática para gerar o número da conta
     private static int numberOfAccounts = 1;
 
-    public Account(String agency) {
+    public Account(String agency, Client client, Notifications notifications) {
         this.agency = agency;
         this.accountNumber = generateAccountNumber();
-    }
-
-    private String generateAccountNumber() {
-        String accountNumber = String.format("%04d", numberOfAccounts);
-        numberOfAccounts++;
-
-        return accountNumber;
+        this.client = client;
+        this.notifications = notifications;
     }
 
     public abstract void transfer(Account destinationAccount, double value);
@@ -26,19 +30,47 @@ public abstract class Account {
 
     public abstract void withdraw(double value);
 
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+    
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+    
+    public void setNotifications(Notifications notifications) {
+        this.notifications = notifications;
+    }
+    
     public String getAgency() {
         return agency;
     }
-
+    
     public String getAccountNumber() {
         return accountNumber;
     }
-
+    
     public Client getClient() {
         return client;
     }
-
+    
     public double getBalance() {
         return balance;
+    }
+    
+    private String generateAccountNumber() {
+        String accountNumber = String.format("%04d", numberOfAccounts);
+        numberOfAccounts++;
+
+        return accountNumber;
+    }
+    
+    @Override
+    public String toString() {
+        return """
+                Agência = %s
+                Número  = %s
+                Usuário = %s
+                Saldo   = %s""".formatted(agency, accountNumber, client.getName(), balance);
     }
 }
